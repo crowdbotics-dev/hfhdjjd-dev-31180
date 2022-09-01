5c87dea29d8c1361c9215c619ac3740023febbe6
Crowdbotics <engineering@crowdbotics.com>
2022-09-01 05:29:34 +0000

    Update models

--- backend/home/admin.py
+++ backend/home/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Aghcjf
+
+admin.site.register(Aghcjf)
 
 # Register your models here.
--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,7 @@
+from django.conf import settings
 from django.db import models
+
+
+class Aghcjf(models.Model):
+    "Generated Model"
+    namretr = models.BigIntegerField()
